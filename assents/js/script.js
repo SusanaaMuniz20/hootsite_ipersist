8cddcc6898918c90ec88a7a6a22724938260d606
Thaiane Cassetari <64805045+thaiicassetari@users.noreply.github.com>
2024-02-16 16:24:34 -0300

    Delete assents directory

--- assents/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
-    // Here you can add your logic to handle form submission, like sending an AJAX request or validating inputs
-    alert('Formulário enviado!'); // For demonstration, just showing an alert
-  });
-  
